8b013a7ae09694479373b356b2a87f0a647afd55
Rocío MR <romuro18@gmail.com>
2022-02-09 22:16:05 +0100

    Obtengo actores y estudios de bbdd con formulario

--- src/app/components/modal-form/modal-form.component.ts
+++ src/app/components/modal-form/modal-form.component.ts
@@ -15,18 +15,24 @@ import { MoviesAppService} from '../../services/movies-app.service';
 })
 export class ModalFormComponent implements OnInit {
   form;
+  invert = false;
+  punctuation = 1;
+  actorsList: any = [];
+  studiosList: any = [];
   separatorKeysCodes: number[] = [ENTER, COMMA];
-  fruitCtrl = new FormControl();
-  filteredFruits: Observable<string[]>;
-  fruits: string[] = ['Lemon'];
-  allFruits: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+  studioCtrl = new FormControl();
+  filteredStudios: Observable<string[]>;
+  studios: string[] = ['Ejemplo'];
+  allStudios: string[] = [];
+  actorCtrl = new FormControl();
+  filteredActors: Observable<string[]>;
+  actors: string[] = ['Ejemplo'];
+  allActors: string[] = [];
+  disableSelect = new FormControl(false);
 
-  @ViewChild('fruitInput') fruitInput!: ElementRef<HTMLInputElement>;
-  constructor(private formBuilder: FormBuilder, private element: ElementRef, private actorsService: MoviesAppService){
-    this.filteredFruits = this.fruitCtrl.valueChanges.pipe(
-      startWith(null),
-      map((fruit: string | null) => (fruit ? this._filter(fruit) : this.allFruits.slice())),
-    );
+  @ViewChild('actorInput') actorInput!: ElementRef<HTMLInputElement>;
+  constructor(private formBuilder: FormBuilder, private element: ElementRef, private moviesService: MoviesAppService){
+    //this.filteredActors = this.actorCtrl.valueChanges;
     this.form = formBuilder.group({
       title: ['', Validators.required],
       poster: ['', Validators.required],
@@ -35,18 +41,66 @@ export class ModalFormComponent implements OnInit {
       studio: ['', Validators.required],
       year: ['', Validators.required],
       duration: ['', Validators.required],
-      punctuation: ['', Validators.required]
+      punctuation: [this.punctuation, Validators.required]
     });
   }
   ngOnInit() {
     console.warn("OnInit")
     // alert('hiii')
-     let actors = this.actorsService.getActors().then( data => {
-      console.log('data', data)
-     });
-     console.log('actors', actors)
+    this.moviesService.getActorsList().then( data => {
+      console.log('actores', data);
+      this.actorsList = data;
+      if(this.actorsList){
+        this.getNamesActors();
+        this.filteredActors = this.actorCtrl.valueChanges.pipe(
+          startWith(null),
+          map((actor: string | null) => (actor ? this._filter(actor) : this.allActors.slice())),
+        );
+      }
+    });
+
+    this.moviesService.getStudiosList().then( data => {
+      console.log('actores', data);
+      this.studiosList = data;
+      if(this.studiosList){
+        this.getStudiosList();
+        this.filteredStudios = this.studioCtrl.valueChanges.pipe(
+          startWith(null),
+          map((studio: string | null) => (studio ? this._filter(studio) : this.allStudios.slice())),
+        );
+      }
+    });
+
+  }
+  /**
+  * Method gets all names of actors
+  */
+  getNamesActors() {
+    let name, firstname, lastname;
+    this.actorsList.forEach((actor: any) => {
+      console.log(actor);
+      firstname = actor.first_name;
+      lastname = actor.last_name;
+      name = firstname.concat(' ', lastname);
+      this.allActors.push(name)
+      console.warn('name', name);
+      
+    });
+    console.warn('Actores: ', this.allActors);
   }
 
+  /**
+  * Method gets all names of studios
+  */
+   getStudiosList() {
+    this.studiosList.forEach((studio: any) => {
+      console.log(studio);
+      this.allStudios.push(studio.name);      
+    });
+    console.warn('Estudios: ', this.allStudios);
+  }
+  
+  
   submit() {
     if (this.form.valid) {
       console.log(this.form.value)
@@ -56,38 +110,45 @@ export class ModalFormComponent implements OnInit {
     }
   }
 
+  /**
+  * Method add actors
+  * @param event 
+  */
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
 
-    // Add our fruit
+    // Add our actor
     if (value) {
-      this.fruits.push(value);
+      this.actors.push(value);
     }
 
     // Clear the input value
     event.chipInput!.clear();
 
-    this.fruitCtrl.setValue(null);
+    this.actorCtrl.setValue(null);
   }
 
-  remove(fruit: string): void {
-    const index = this.fruits.indexOf(fruit);
+  /**
+   * Method remove actor
+   * @param actor 
+   */ 
+  remove(actor: string): void {
+    const index = this.actors.indexOf(actor);
 
     if (index >= 0) {
-      this.fruits.splice(index, 1);
+      this.actors.splice(index, 1);
     }
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.fruits.push(event.option.viewValue);
-    this.fruitInput.nativeElement.value = '';
-    this.fruitCtrl.setValue(null);
+    this.actors.push(event.option.viewValue);
+    this.actorInput.nativeElement.value = '';
+    this.actorCtrl.setValue(null);
   }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-
-    return this.allFruits.filter(fruit => fruit.toLowerCase().includes(filterValue));
+    return this.allActors.filter(actor => actor.toLowerCase().includes(filterValue));
   }
 }
 
