4e750ad8779e8de47ccf2dd60150b26a2613e32d
Rocío MR <romuro18@gmail.com>
2022-02-28 22:32:56 +0100

    Borrar película

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingService } from 'src/app/shared/loading/loading.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  title ="Home"
-  constructor() { }
+  title ="Home";
+  constructor(private loadingService: LoadingService,) { }
 
   ngOnInit(): void {
+    
+    this.loadingService.showLoader();
+
   }
 
 }
